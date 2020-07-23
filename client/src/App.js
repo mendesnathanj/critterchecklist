7f27d39756b21ba932c1fca2f7f692c3208b4454
Nathan <mendesnathanj@gmail.com>
2020-07-23 11:36:30 -0700

    Initialize client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from './utils/route';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import TestPage from './pages/TestPage';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <AuthRoute path='/login' component={Login} />
-        <Route path='/test_page' component={TestPage} />
-        <ProtectedRoute path='/' component={Home} />
-      </Switch>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import configureStore from "./store/store";
-import getPreloadedState from './utils/preloadedState';
-
-localStorage.removeItem('token');
-
-const store = configureStore(getPreloadedState());
-window.store = store;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
